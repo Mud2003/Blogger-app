2fb3bc9b10f7ff623fd97b942695eb42bbd8357d
sonicprv <>
2023-04-27 13:26:54 +0530

    Responsive NavBar created.

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import './navbar.css';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from 'react';
+
+function NavBar() {
+
+    const [display, setDisplay] = useState('none');
+
+    const clickBtn = () => {
+        // setDisplay('flex');
+        setDisplay((prevDisplay) => (prevDisplay === 'none' ? 'flex' : 'none'));
+    }
+
+    return (
+        <>
+            <nav className='navbar'>
+                <h1 className='logo'>BLOG</h1>
+                <ul className='links'>
+                    <li id='ll'><Link>Home</Link></li>
+                    <li id='ll'><Link>Blogs</Link></li>
+                    <li id='ll'><Link>Create Post</Link></li>
+                </ul>
+                <div className='menuicon' onClick={clickBtn}>
+                    <MenuIcon fontSize='medium' style={{ display: display === 'none' ? 'block' : 'none' }} />
+                    <CloseIcon fontSize='medium' style={{ display: display === 'none' ? 'none' : 'block' }} />
+                </div>
+            </nav>
+            <ul className="menulist" id='mnl' style={{display}}>
+                <li id='ml'><Link>Home</Link></li>
+                <li id='ml'><Link>Blogs</Link></li>
+                <li id='ml'><Link>Create Post</Link></li>
+            </ul>
+        </>
+    )
+}
+
+export default NavBar;
---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './home.css';
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Welcome</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad expedita mollitia obcaecati neque consequatur porro! Doloribus atque laboriosam quisquam? Nesciunt rem earum nemo voluptatibus laboriosam? Accusamus officia odio saepe rem facilis, expedita aliquam asperiores quaerat quod veritatis eligendi quisquam nam! Eius velit laboriosam quibusdam soluta non, voluptas inventore vero perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad expedita mollitia obcaecati neque consequatur porro! Doloribus atque laboriosam quisquam? Nesciunt rem earum nemo voluptatibus laboriosam? Accusamus officia odio saepe rem facilis, expedita aliquam asperiores quaerat quod veritatis eligendi quisquam nam! Eius velit laboriosam quibusdam soluta non, voluptas inventore vero perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad expedita mollitia obcaecati neque consequatur porro! Doloribus atque laboriosam quisquam? Nesciunt rem earum nemo voluptatibus laboriosam? Accusamus officia odio saepe rem facilis, expedita aliquam asperiores quaerat quod veritatis eligendi quisquam nam! Eius velit laboriosam quibusdam soluta non, voluptas inventore vero perferendis! Maiores suscipit possimus magnam commodi, sed cupiditate cumque nihil in.</p>
+        </div>
+    )
+}
+
+export default Home;
