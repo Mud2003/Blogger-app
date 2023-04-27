2be0a8b7603b3fd7cd18e48484ae37f79da62e82
sonicprv <>
2023-04-27 13:28:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
