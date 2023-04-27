4eb190f1c43e70167d75e4ad520ceee7bb4325d8
sonicprv <>
2023-04-27 13:53:15 +0530

    Home component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Home from './components/Pages/Home/Home'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <NavBar/>
-        {/* <Home/> */}
+        <Home/>
       </BrowserRouter>
     </div>
   );
