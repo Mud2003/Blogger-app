72cf485812913b4f00fc2327f05d26120684aefb
Muditha <kabmmadusith2003@gmail.com>
2023-05-10 16:55:05 +0530

    update

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import "./login.css";
+import axios from "axios";
+import {  useState } from 'react';
 
 export default function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+   // Send a POST request to the backend endpoint with the email and password input values
+    try {
+      await axios.post('http://localhost:5000/api/users/login', { email, password });
+      alert('Login successful'); // Output: 'Login successful',
+    } catch (error) {
+      alert("Error in occured"); // Output: 'Invalid email or password'
+    }
+  };
+
   return (
-        <form>
+      <form onSubmit={handleSubmit}>
           <h3>Login Here</h3>
 
-          <label for="username">Username</label>
-          <input type="text" placeholder="Email" id="username"/>
+          <label for="username">Email</label>
+          <input type="text" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-              <label for="password">Password</label>
-              <input type="password" placeholder="Password" id="password"/>
+          <label for="password">Password</label>
+          <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-                  <button>Log In</button>
-        </form>      
+          <button type='submit'>Log In</button>
+      </form>      
   )
 }
--- src/components/Pages/Register/Register.jsx
+++ src/components/Pages/Register/Register.jsx
@@ -1,18 +1,65 @@
 import React from 'react';
+import {  useState } from 'react';
 import'./register.css';
+import axios from "axios";
+
 
 export default function Register() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+
+
+    function handleSubmit(e) {
+        e.preventDefault(); // prevent page from refreshing
+
+        const newUser = {
+            name,
+            email,
+            password,
+            password2
+        }
+
+        setName('');
+        setEmail('');
+        setPassword('');
+        setPassword2('');
+
+
+        axios.post("http://localhost:5000/api/users/register", newUser).then(()=>{
+            alert("User registration successful...")
+        }).catch((err)=>{
+            alert(err)
+        })
+    };
+
   return (
-      <form className='r'>
+      <form className='r' onSubmit={handleSubmit}>
           <h3>Register Here</h3>
 
           <label for="username">Username</label>
-          <input type="text" placeholder="Email" id="username" />
+          <input type="text" placeholder="Name" id="username" value={name} onChange={(e) => {
+              setName(e.target.value)
+          }} />
 
-          <label for="password">Password</label>
-          <input type="password" placeholder="Password" id="password" />
+          <label for="username">Email</label>
+          <input type="text" placeholder="Email" id="email" value={email} onChange={(e) => {
+              setEmail(e.target.value)
+          }} />
 
-          <button>Register</button>
+          <label for="username">Password</label>
+          <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => {
+              setPassword(e.target.value)
+          }} />
+
+          <label for="password">Confirm Password</label>
+          <input type="password" placeholder="Password" id="password2" value={password2} onChange={(e) => {
+              setPassword2(e.target.value)
+          }} />
+
+          <button type='submit'>Register</button>
       </form>
   )
 }
